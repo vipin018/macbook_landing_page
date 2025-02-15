80c3c5c6c70a8967f7b23a4a427391e2f182f427
vipin018 <vipinpathak018@gmail.com>
2025-02-15 19:14:51 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { ScrollControls } from '@react-three/drei'
+// import { OrbitControls } from '@react-three/drei'
 import Macbook from './Macbook'
 import { Environment } from '@react-three/drei'
 const App = () => {
   return (
     <Canvas camera={{ fov:20, position: [0, -1, 100 ] }}>
-      <OrbitControls />
+      {/* <OrbitControls /> */}
       <Environment 
       files="https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/1k/studio_small_09_1k.exr"
       />
-      <Macbook />
+      <ScrollControls pages={3}>
+        <Macbook />
+      </ScrollControls>
     </Canvas>
   )
 }
--- src/Macbook.jsx
+++ src/Macbook.jsx
@@ -1,26 +1,38 @@
-import { useGLTF } from '@react-three/drei'
+import { useGLTF, ScrollControls, useScroll, useTexture } from '@react-three/drei'
 import React from 'react'
 import * as THREE from 'three';
+import { useFrame } from '@react-three/fiber';
+
 const Macbook = () => {
-    let model = useGLTF('src/assets/mac.glb');
+let texture = useTexture('/red.jpg');
+
+
+    let model = useGLTF('/mac.glb');
     //   the below code is used to get all the meshes in the model and group them by name like in this case the name of the mesh is "Screen" and "Body" and then we can use them to animate the model
     let meshes = {};
     model.scene.traverse((child) => {
-
         meshes[child.name] = child;
-
     });
     // console.log(meshes);
     // the below code is used to animate the model this will rotate the screen of the macbook to 180 degrees which make it look like that laptop is closed.
-    meshes.screen.rotation.x = Math.PI;
+    meshes.screen.rotation.x = THREE.MathUtils.degToRad(180);
+    meshes.matte.material.map = texture;
+    meshes.matte.material.emissive = new THREE.Color(0.5, 0, 0);
+    meshes.matte.material.emissiveIntensity = 1;
+    meshes.matte.material.metalness = 0;
+    meshes.matte.material.roughness = 1;
+   let scroll = useScroll();
+   useFrame((state, delta) => {
+    meshes.screen.rotation.x = THREE.MathUtils.degToRad(180 - scroll.offset * 90);
+   })
 
-        return(
-            <>
-                <group position={[0, -10, 0]}>
-                    <primitive object={model.scene} />
-                </group>
-            </>
-        )
+    return (
+        <>
+            <group position={[0, -10, 0]}>
+                <primitive object={model.scene} />
+            </group>
+        </>
+    )
 }
 
 export default Macbook
