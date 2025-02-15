f1a2661f1b2165ca3dc5220ed14c703047d052e4
vipin018 <vipinpathak018@gmail.com>
2025-02-15 19:41:11 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,10 @@ const App = () => {
   return (
     <>
       <div className='w-full h-screen bg-gray-300'>
-        <div className='absolute top-16 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-          <h3 className='text-7xl font-semibold'>Macbook Pro.</h3>
-          
+        <div className='absolute flex flex-col items-center gap-2 top-32 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+          <h3 className=' masked text-9xl font-semibold'>Macbook Pro.</h3>
+          <h5 className='text-2xl font-semibold'>Hello, Apple Intelligence.</h5>
+          <p className='text-md opacity-50 w-2/3 text-center'>Available in 13-inch and 15-inch sizes with Retina display and Touch Bar.</p>
         </div>
         <Canvas camera={{ fov: 20, position: [0, -1, 100] }}>
           {/* <OrbitControls /> */}
